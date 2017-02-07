00fef4b4c2f3e657781e5adc25f6478f3927fecc
gtj <getianjie@nicigo.com>
2017-02-07 13:36:12 +0800

    1. add 2 method :  findpage 、 findtitle

--- tiebaSpider.py
+++ tiebaSpider.py
@@ -10,44 +10,67 @@
 # ---------------------------------------
 
 import urllib2
+import re
+
 
 class HtmlToo:
     pass
 
 
 class TiebaSpider:
-
         # 构造函数 , 初始化参数
         def __init__(self, url):
             # 默认只看楼主
             self.myurl = url + '?see_lz=1'
+            self.page = 0
+            self.title = ""
             pass
 
-        # 寻找标题函数
-        def total_page(self, page):
-            urlsf="sdfs"
-            pass
+        # 寻找标题函数  .  <span class="red">163</span>
+        def total_page(self, content):
+            if content is None:
+                return
+            match = re.search(r'class="red">(\d+?)</span>', content, re.S)
+            if match:
+                page = int(match.group(1))
+                print u'爬虫报告：楼主内容一共有%d'% page
+            else:
+                page = 0
+                print u'爬虫报告:楼主未发布任何内容'
+            return page
 
         # 抓取帖子内容函数
-        def fetch_data(self):
-            page = urllib2.urlopen(self.myurl).read().decode("utf-8")
-            self.total_page(page)
+        def collect_data(self):
+            content = urllib2.urlopen(self.myurl).read().decode("utf-8")
+            # 抓取帖子总页数
+            self.page = self.total_page(content)
+            # 抓取标题
+            self.title = self.find_title(content)
             pass
 
         # 寻找标题函数
-        def find_title(self):
-            pass
-            pass
+        def find_title(self, content):
+            if content is None:
+                return
+            match = re.search(r'<title>(.*?)</title>', content, re.S)
+            if match:
+                title = match.group(1)
+            else:
+                title = u'暂无标题'
+            print u'爬虫报告：标题是 ' + title
+            return title
 
 
-print u"""#百度贴吧爬虫启动...
-请输入帖子最后一串数字..
+print u"""
+#百度贴吧爬虫启动...
+#请输入帖子最后一串数字..
  """
+
 # 获取帖子url , 用户需要输入尾部数字串
 myurl = "http://tieba.baidu.com/p/" + str(raw_input("http://tieba.baidu.com/p/"))
 
 spider = TiebaSpider(myurl)
-spider.fetch_data()
+spider.collect_data()
 
 
 
